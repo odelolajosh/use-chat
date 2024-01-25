591809a865f51a1eb22b3250714d2d673bb2f9ba
Joshua Odelola <oladapoodelola@gmail.com>
2024-01-25 12:54:06 +0100

    feat: add connection status to event

--- src/events/ConnectionStateChangedEvent.ts
+++ src/events/ConnectionStateChangedEvent.ts
@@ -1,7 +1,12 @@
-import { ChatEventType } from "../enums";
+import { ChatEventType, ConnectionState } from "../enums";
 import { ChatEvent } from "./ChatEvent";
 
 export class ConnectionStateChangedEvent
   implements ChatEvent<ChatEventType.ConnectionStateChanged> {
   readonly type = ChatEventType.ConnectionStateChanged;
+  status: ConnectionState;
+  
+  constructor(status: ConnectionState) {
+    this.status = status;
+  }
 }
